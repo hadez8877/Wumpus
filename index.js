8af6594dc02e8d4bd3dc750e633701c4f824c22f
hadez8877 <hadezstreaming@gmail.com>
2024-07-07 22:47:30 -0500

    🚩 Unnecessary imports removed

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-const { Client, GatewayIntentBits, Partials, Collection, Events, EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder, ButtonBuilder, ButtonStyle, ReactionEmoji } = require("discord.js");
+const { Client, GatewayIntentBits, Partials, Collection } = require("discord.js");
 const { loadEvents } = require("./Handlers/eventHandler");
+
 require('dotenv').config();
 
-  const { Guilds, GuildMembers, GuildMessages, MessageContent } = GatewayIntentBits;
-  const { User, Message, GuildMember, ThreadMember } = Partials;
-  
-  const client = new Client({
-    intents: [Guilds, GuildMembers, GuildMessages, MessageContent],
-    partials: [User, Message, GuildMember, ThreadMember],
-  });
-  
-  client.events = new Collection();
-  client.commands = new Collection();
-  client.prefixs = new Collection();
-  
-  loadEvents(client);
+const { Guilds, GuildMembers, GuildMessages, MessageContent } = GatewayIntentBits;
+const { User, Message, GuildMember, ThreadMember } = Partials;
 
-  client.login(process.env.TOKEN);
+const client = new Client({
+  intents: [Guilds, GuildMembers, GuildMessages, MessageContent],
+  partials: [User, Message, GuildMember, ThreadMember],
+});
+
+client.events = new Collection();
+client.commands = new Collection();
+client.prefixs = new Collection();
+
+loadEvents(client);
+
+client.login(process.env.TOKEN);
