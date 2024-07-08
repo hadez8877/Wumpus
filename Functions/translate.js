cdbd9062071108e7d8dccc28fc7ab079c6f9d64e
hadez8877 <hadezstreaming@gmail.com>
2024-07-07 22:53:50 -0500

    💡 automatic translation function

---
+++ Functions/translate.js
@@ -0,0 +1,15 @@
+const translate = require("translate-google");
+const subscribe = require("../Schemas/subscribeSchema");
+
+async function sendTranslated(text, guildId) {
+  if (!guildId) throw new Error("Gulid ID required!");
+
+  const data = await subscribe.findOne({ Guild: guildId });
+  const language = data?.Language || "es";
+
+  let result = await translate(text, { to: language });
+
+  return result;
+}
+
+module.exports = { sendTranslated };
