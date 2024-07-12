66cfbc509625ff2b850f081762ddd044fb492eba
hadez8877 <hadezstreaming@gmail.com>
2024-07-12 18:39:57 -0500

    ✏️ Fixed ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,22 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 export default [
   {
-    files: ["**/*.js"],
+    files: ['**/*.js'],
     languageOptions: {
-      sourceType: "commonjs",
+      sourceType: 'commonjs',
       globals: {
         ...globals.node,
         ...globals.browser,
       },
     },
     rules: {
-      "no-console": "off",
-      "no-unused-vars": "off",
-      "eqeqeq": "error",
-      "no-var": "off",
-      "prefer-const": "error"
+      'no-console': 'off',
+      'no-unused-vars': 'warn',
+      "eqeqeq": 'error',
+      'no-var': 'off',
+      'prefer-const': 'warn',
     },
   },
   pluginJs.configs.recommended,
