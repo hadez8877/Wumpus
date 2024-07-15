83c50116dcb14e7a13a88e99e42505e9709f3967
hadez8877 <hadezstreaming@gmail.com>
2024-07-15 16:50:58 -0500

    ⚡fixed config vitest

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    testTimeout: 10000
+    testTimeout: 10000,
+    environment: 'node',
+    include: ['src/**/*.test.{js,mjs,ts}']
   },
 });
