5456bca0264c800be47048d9afc6db762169151f
hadez8877 <hadezstreaming@gmail.com>
2024-08-02 18:38:35 -0500

    Added new classes extends of `Error`

---
+++ src/utils/errors/DBError.ts
@@ -0,0 +1,53 @@
+import util from "util";
+import labelType from "../labels";
+import kleur from "kleur";
+
+type ErrorCodes = 12002 | 11000 | 121 | 50 | 6 | 89 | 94;
+
+class DBError extends Error {
+    code: ErrorCodes;
+
+    constructor(code: ErrorCodes) {
+        // eslint-disable-next-line no-var
+        var message: string;
+
+        switch (code) {
+            case 12002:
+                message = `${kleur.yellow("[12002]")} Connection failed. Unable to connect to MongoDB.`;
+            break;
+            case 11000:
+                message = `${kleur.yellow("[11000]")} Duplicate key error. The document you are trying to insert already exists.`;
+            break;
+            case 121:
+                message = `${kleur.yellow("[121]")} Document validation failure. The document does not meet validation criteria.`;
+            break;
+            case 50:
+                message = `${kleur.yellow("[50]")} Server selection timeout. The client could not find a suitable server.`;
+            break;
+            case 6:
+                message = `${kleur.yellow("[6]")} Not master error. The operation is not allowed on this node.`;
+            break;
+            case 89:
+                message = `${kleur.yellow("[89]")} Write concern error. The write concern could not be fulfilled.`;
+            break;
+            case 94:
+                message = `${kleur.yellow("[94]")} Network error. There was a network error during the operation.`;
+            break;
+            default: {
+                message = `${labelType.ERROR} An unknown error occurred with code ${code}.`;
+            }
+        }
+
+        super(message);
+        this.code = code;
+        this.name = "DBError";
+
+        Error.captureStackTrace(this, DBError);
+    }
+
+    [util.inspect.custom]() {
+        return `${labelType.ERROR} Database encountered an issue. ${this.code ? `Error Code: ${kleur.bold().yellow(`${this.code}`)}` : "No Error Code"}. Error Message: ${this.message}`;
+    }
+}
+
+export default DBError;
--- src/utils/errors/DiscordAPIError.ts
+++ src/utils/errors/DiscordAPIError.ts
@@ -2,14 +2,54 @@ import util from "util";
 import labelType from "../labels";
 import kleur from "kleur";
 
-class DiscordAPIError extends Error {
-  code: number | string | undefined;
-  shardId: number;
+type ErrorCodes = 400 | 401 | 403 | 404 | 405 | 429 | 500 | 502 | 503 | 504;
 
-  constructor(message: string, code: number | string | undefined, shardId: number) {
+class DiscordAPIError extends Error {
+  code: ErrorCodes;
+
+  constructor(code: ErrorCodes) {
+    // eslint-disable-next-line no-var
+    var message: string;
+
+    switch (code) {
+      case 400:
+        message = `${kleur.yellow("[400]")} Bad Request. The request was invalid or cannot be otherwise served.`;
+      break;
+      case 401:
+        message = `${kleur.yellow("[401]")} Unauthorized. Invalid or missing authentication token.`;
+      break;
+      case 403:
+        message = `${kleur.yellow("[403]")} Forbidden. You do not have permission to perform this action.`;
+      break;
+      case 404:
+        message = `${kleur.yellow("[404]")} Not Found. The requested resource was not found.`;
+      break;
+      case 405:
+        message = `${kleur.yellow("[405]")} Method Not Allowed. The method specified in the request is not allowed.`;
+      break;
+      case 429:
+        message = `${kleur.yellow("[429]")} Too Many Requests. You have sent too many requests in a given amount of time.`;
+      break;
+      case 500:
+        message = `${kleur.yellow("[500]")} Internal Server Error. An unexpected error occurred on the server.`;
+      break;
+      case 502:
+        message = `${kleur.yellow("[502]")} Bad Gateway. The server received an invalid response from the upstream server.`;
+      break;
+      case 503:
+        message = `${kleur.yellow("[503]")} Service Unavailable. The server is currently unable to handle the request.`;
+      break;
+      case 504:
+        message = `${kleur.yellow("[504]")} Gateway Timeout. The server did not receive a timely response from the upstream server.`;
+      break;
+
+      default: {
+        message = `${labelType.ERROR} Unknown error with code ${kleur.yellow(`${code}`)}.`;
+      }
+    }
+
     super(message);
     this.code = code;
-    this.shardId = shardId;
 
     this.name = "DiscordAPIError";
 
@@ -17,7 +57,7 @@ class DiscordAPIError extends Error {
   }
 
   [util.inspect.custom]() {
-    return `${labelType.ERROR} Discord API encountered an issue. ${this.code ? `Error Code: ${kleur.bold().yellow(`${this.code}`)}` : "No Error Code"}. Shard ID: ${kleur.bold().blue(`${this.shardId}`)}. Error Message: ${kleur.red(this.message)}`;
+    return `${labelType.ERROR} Discord API encountered an issue. ${this.code ? `Error Code: ${kleur.bold().yellow(`${this.code}`)}` : "No Error Code"}. Error Message: ${this.message}`;
   }
 }
 
