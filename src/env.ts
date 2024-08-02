0aa2d2385338537a7a7763e3964248b7cb8925c3
hadez8877 <hadezstreaming@gmail.com>
2024-08-02 18:26:54 -0500

    Update environment variables

---
+++ src/env.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+import "dotenv/config";
+
+const envType = z.object({
+    CLIENT_ID: z.string().min(16),
+    CLIENT_SECRET: z.string().length(32),
+
+    BOT_TOKEN: z.string().min(50),
+    BOT_PREFIX: z.string().optional().default("!"),
+
+    DB_PORT: z.string().regex(/^mongodb\+srv:\/\/[^:]+:[^@]+@[^\\/]+\/\?[^\\/]+$/)
+});
+
+envType.parse(process.env);
+
+declare global {
+    // eslint-disable-next-line @typescript-eslint/no-namespace
+    namespace NodeJS {
+        interface ProcessEnv extends z.infer<typeof envType> {}
+    }
+}
