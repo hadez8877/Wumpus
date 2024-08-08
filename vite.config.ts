ace0da7c434bd48f36e58602de441daeaea0e755
hadez8877 <hadezstreaming@gmail.com>
2024-08-08 16:10:55 -0500

    Changed from Vitest settings, now aliases work

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vitest/config";
+import path from "path"
 
 export default defineConfig({
   test: {
-    testTimeout: 60000
+    environment: "node",
+    testTimeout: 20000
   },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, './src/'),
+      "@db/": path.resolve(__dirname, './db/')
+    },
+  }
 });
