51d2e098d79fb60da543e1fbbecbea232da80298
C:/Program Files/Git/devs/hadez <HadezStreaming@gmail.com>
2024-08-14 22:59:24 -0500

    Add utility functions for message formatting and URL extraction
    - Added `successMessage` and `errorMessage` functions to format messages with optional emojis.
    
    - Implemented `GuildInvite` and `GroupDMInvite` types for invite information.
    
    - Created `getUrlsInString` function to extract URLs from a string, with an option for uniqueness.
    
    - Added `getInviteCodesInString` function to extract Discord invite codes from various formats.

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
-import { Client, Invite } from "discord.js";
+import { Client, Invite, InviteGuild, Guild, PartialChannelData, ChannelType } from "discord.js";
 import SimpleCache from "@/SimpleCache";
+import tlds from "tlds";
 
 export const MS = 1;
 export const SECONDS = 1000 * MS;
@@ -50,6 +51,121 @@ export function trimEmptyStartEndLines(str: string) {
   }
 }
 
+export function successMessage(str: string, emoji = "<:MessageSuccess:1273483541585330216>") {
+  return emoji ? `${emoji} ${str}` : str;
+}
+
+export function errorMessage(str: string, emoji = "<:MessageInteractionWarn:1234642336580108298>") {
+  return emoji ? `${emoji} ${str}` : str;
+}
+
+export type GuildInvite = Invite & { guild: InviteGuild | Guild };
+export type GroupDMInvite = Invite & {
+  channel: PartialChannelData;
+  type: typeof ChannelType.GroupDM;
+};
+
+const realLinkRegex = /https?:\/\/\S+/; // http://anything or https://anything
+const plainLinkRegex = /((?!https?:\/\/)\S)+\.\S+/; // anything.anything, without http:// or https:// preceding it
+
+// Both of the above, with precedence on the first one
+const urlRegex = new RegExp(`(${realLinkRegex.source}|${plainLinkRegex.source})`, "g");
+const protocolRegex = /^[a-z]+:\/\//;
+
+interface MatchedURL extends URL {
+  input: string;
+}
+
+export function getUrlsInString(str: string, onlyUnique = false): MatchedURL[] {
+  let matches = [...(str.match(urlRegex) ?? [])];
+  if (onlyUnique) {
+    matches = unique(matches);
+  }
+
+  return matches.reduce<MatchedURL[]>((urls, match) => {
+    const withProtocol = protocolRegex.test(match) ? match : `https://${match}`;
+
+    let matchUrl: MatchedURL;
+    try {
+      matchUrl = new URL(withProtocol) as MatchedURL;
+      matchUrl.input = match;
+    } catch {
+      return urls;
+    }
+
+    let hostname = matchUrl.hostname.toLowerCase();
+
+    if (hostname.length > 3) {
+      hostname = hostname.replace(/[^a-z]+$/, "");
+    }
+
+    const hostnameParts = hostname.split(".");
+    const tld = hostnameParts[hostnameParts.length - 1];
+    if (tlds.includes(tld)) {
+      urls.push(matchUrl);
+    }
+
+    return urls;
+  }, []);
+}
+
+// discord.com/invite/<code>
+// discordapp.com/invite/<code>
+// discord.gg/invite/<code>
+// discord.gg/<code>
+// discord.com/friend-invite/<code>
+const quickInviteDetection =
+  /discord(?:app)?\.com\/(?:friend-)?invite\/([a-z0-9-]+)|discord\.gg\/(?:\S+\/)?([a-z0-9-]+)/gi;
+
+const isInviteHostRegex = /(?:^|\.)(?:discord.gg|discord.com|discordapp.com)$/i;
+const longInvitePathRegex = /^\/(?:friend-)?invite\/([a-z0-9-]+)$/i;
+
+export function getInviteCodesInString(str: string): string[] {
+  const inviteCodes: string[] = [];
+
+  // Clean up markdown
+  str = str.replace(/[|*_~]/g, "");
+
+  // Quick detection
+  const quickDetectionMatch = str.matchAll(quickInviteDetection);
+  if (quickDetectionMatch) {
+    inviteCodes.push(...[...quickDetectionMatch].map((m) => m[1] || m[2]));
+  }
+
+  // Deep detection via URL parsing
+  const linksInString = getUrlsInString(str, true);
+  const potentialInviteLinks = linksInString.filter((url) => isInviteHostRegex.test(url.hostname));
+  const withNormalizedPaths = potentialInviteLinks.map((url) => {
+    url.pathname = url.pathname.replace(/\/{2,}/g, "/").replace(/\/+$/g, "");
+    return url;
+  });
+
+  const codesFromInviteLinks = withNormalizedPaths
+    .map((url) => {
+      // discord.gg/[anything/]<code>
+      if (url.hostname === "discord.gg") {
+        const parts = url.pathname.split("/").filter(Boolean);
+        return parts[parts.length - 1];
+      }
+
+      // discord.com/invite/<code>[/anything]
+      // discordapp.com/invite/<code>[/anything]
+      // discord.com/friend-invite/<code>[/anything]
+      // discordapp.com/friend-invite/<code>[/anything]
+      const longInviteMatch = url.pathname.match(longInvitePathRegex);
+      if (longInviteMatch) {
+        return longInviteMatch[1];
+      }
+
+      return null;
+    })
+    .filter(Boolean) as string[];
+
+  inviteCodes.push(...codesFromInviteLinks);
+
+  return unique(inviteCodes);
+}
+
 const inviteCache = new SimpleCache<Promise<Invite | null>>(10 * MINUTES, 200);
 
 type ResolveInviteReturnType = Promise<Invite | null>;
@@ -68,4 +184,8 @@ export async function resolveInvite<T extends boolean>(
   inviteCache.set(key, promise);
 
   return promise as ResolveInviteReturnType;
+}
+
+export function unique<T>(arr: T[]): T[] {
+  return Array.from(new Set(arr));
 }
